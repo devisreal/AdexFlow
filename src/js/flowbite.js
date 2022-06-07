1e632f9b1b11f4a480b008a7ca1565040a453599
Israel <akinosoissy@gmail.com>
2022-06-07 04:11:00 +0100

    ScrollReveal Added'

--- src/js/flowbite.js
+++ src/js/flowbite.js
@@ -4045,9 +4045,9 @@ reference element's position.
   var tabs_Default = {
     defaultTabId: null,
     activeClasses:
-      "text-customPurple hover:text-customPurple dark:text-customPurple dark:hover:text-customPurple border-customOrange dark:bordercustomOrange",
+      "text-customPurple hover:text-customPurple dark:text-customPurple dark:hover:text-customPurple border-gray-400 dark:border-gray-400",
     inactiveClasses:
-      "text-gray-500 hover:text-gray-500 dark:text-gray-400 border-gray-100 hover:border-gray-400 dark:border-gray-100 dark:hover:text-gray-500",
+      "text-gray-500 hover:text-gray-500 dark:text-gray-400 border-white hover:border-gray-300 dark:border-white dark:hover:text-gray-500",
     onShow: function onShow() {},
   };
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,3 @@
-window.onscroll = () => {
-  
-};
-
 // Side Nav controls
 $(document).ready(function () {
   $("#show-menu").click(function () {
@@ -19,8 +15,8 @@ $(document).ready(function () {
 });
 
 // ScrollSpy
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("nav a");
 
 window.onscroll = () => {
   if (
@@ -30,23 +26,21 @@ window.onscroll = () => {
     document.getElementById("scroll-nav").style.top = "0px";
   } else {
     document.getElementById("scroll-nav").style.top = "-80px";
-  };
-  sections.forEach(section => {
+  }
+  sections.forEach((section) => {
     let top = window.scrollY;
     let offset = section.offsetTop - 150;
     let height = section.offsetHeight;
-    let id = section.getAttribute('id');
-    
-    if(top >= offset && top < offset + height){
-      navLinks.forEach(link => {
-        link.classList.remove('active');
-        document.querySelector(`nav a[href="#${id}"]`).classList.add('active');
-      })      
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document.querySelector(`nav a[href="#${id}"]`).classList.add("active");
+      });
     }
-  })
-}
-
-
+  });
+};
 
 // Animated Headline
 $(function () {
@@ -67,11 +61,98 @@ var copyright = document.getElementById("copyright");
 copyright.innerHTML = currentYear;
 
 // Birthday Toast
-var myBirthday = new Date()
-myBirthday.setDate(12)
-myBirthday.setMonth(5)
-let today = new Date()
-var birthdayToast = document.getElementById("birthday-toast")
-if (today.getMonth() == myBirthday.getMonth() && today.getDate() == myBirthday.getDate()){
-  birthdayToast.style.display = "flex"
+var myBirthday = new Date();
+myBirthday.setDate(12);
+myBirthday.setMonth(5);
+let today = new Date();
+var birthdayToast = document.getElementById("birthday-toast");
+if (
+  today.getMonth() == myBirthday.getMonth() &&
+  today.getDate() == myBirthday.getDate()
+) {
+  birthdayToast.style.display = "flex";
+} else {
+  birthdayToast.style.display = "none";
 }
+
+//ScrollReveal
+window.sr = ScrollReveal({
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  interval: 16,
+});
+
+sr.reveal(".about-img, .contact-details", {
+  distance: "100px",
+  duration: 1000,
+  origin: "bottom",
+  scale: 0.75,
+  opacity: 0,
+});
+
+sr.reveal(".about-tabs, .jumbo.right, .contact-form", {
+  distance: "100px",
+  duration: 1000,
+  origin: "right",
+  scale: 0.75,
+  opacity: 0,
+  delay: 300,
+});
+
+sr.reveal(".jumbo.left", {
+  distance: "100px",
+  duration: 1000,
+  origin: "left",
+  scale: 0.75,
+  opacity: 0,
+  delay: 500,
+});
+
+// Scrollreveal headings
+sr.reveal(".service-heading, .portfolio-heading, .contact-heading", {
+  distance: "100px",
+  duration: 1000,
+  origin: "bottom",
+  scale: 0.75,
+  opacity: 0,
+  delay: 500,
+});
+
+// Scrollreveal Triple Containers
+// left
+sr.reveal(".service-box.left, .portfolio-box.left", {
+  distance: "100px",
+  duration: 1000,
+  origin: "left",
+  scale: 0.75,
+  opacity: 0,
+  delay: 700,
+});
+
+// center
+sr.reveal(".service-box.center, .portfolio-box.center", {
+  distance: "100px",
+  duration: 1000,
+  origin: "top",
+  scale: 0.75,
+  opacity: 0,
+  delay: 1200,
+});
+
+// right
+sr.reveal(".service-box.right, .portfolio-box.right", {
+  distance: "100px",
+  duration: 1000,
+  origin: "right",
+  scale: 0.75,
+  opacity: 0,
+  delay: 1700,
+});
+
+// Footer
+sr.reveal(".footer-link", {
+  interval: 30,
+  duration: 1200,
+  origin: "bottom",
+  scale: 0.75,
+  opacity: 0,
+});
